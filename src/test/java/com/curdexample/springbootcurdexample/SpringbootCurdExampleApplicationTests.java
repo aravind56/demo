ba4c7e73a108b68f018e57267e6e5a3946ae6610
prashant <prashantckundagol@gmail.com>
2024-03-20 06:59:59 +0530

    First commit

---
+++ src/test/java/com/curdexample/springbootcurdexample/SpringbootCurdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curdexample.springbootcurdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCurdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
